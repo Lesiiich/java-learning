20178edba5aec6b273ae6712ec344ca74183e541
Olesia Shefer <olesyaolegovna1919@gmail.com>
2022-01-16 21:45:44 +0300

    Change message to print

--- src/main/java/com/github/lesiiich/java_learning/Main.java
+++ src/main/java/com/github/lesiiich/java_learning/Main.java
@@ -2,6 +2,6 @@ package com.github.lesiiich.java_learning;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello Lesia");
     }
 }
