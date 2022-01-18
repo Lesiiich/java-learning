8db4f5e6a31c2fcb69e62892aa15c621770c8048
Olesia Shefer <olesyaolegovna1919@gmail.com>
2022-01-18 21:20:21 +0300

    ACMP 0001

---
+++ src/main/java/com/github/lesiiich/java_learning/acmp/task0001/Main.java
@@ -0,0 +1,20 @@
+package com.github.lesiiich.java_learning.acmp.task0001;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            PrintWriter out = new PrintWriter(System.out);
+
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int c=a+b;
+            out.print(c);
+            out.flush();
+        }
+    }
+
+
+
